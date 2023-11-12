447c842931f7198dbd8c27255b9e6954b47ccce8
Mike Slinn <mslinn@mslinn.com>
2023-11-12 10:37:47 -0500

    Initial commit

--- lib/badge.rb
+++ lib/badge.rb
@@ -1,12 +1,49 @@
-require 'jekyll'
-require 'jekyll_plugin_logger'
 require 'jekyll_plugin_support'
+require_relative 'badge/version.rb'
 
-require_relative 'badge/version'
+# This Jekyll tag plugin is a minimal example.
+#
+# See https://www.mslinn.com/jekyll/10200-jekyll-plugin-background.html
+# See https://www.mslinn.com/jekyll/10400-jekyll-plugin-template-collection.html
+#
+# @example Heading for this example
+#   {% badge param1='value1' %}
+#
+# The Jekyll log level defaults to :info, which means all the Jekyll.logger statements below will not generate output.
+# You can control the log level when you start Jekyll.
+# To set the log level to :debug, write an entry into _config.yml, like this:
+# plugin_loggers:
+#   Badge: debug
+module Badge
+  # This class implements the Jekyll badge functionality
+  class Badge < JekyllSupport::JekyllTag
+    PLUGIN_NAME = 'badge'.freeze
+    VERSION = Badge::VERSION
 
-# Require all Ruby files in 'lib/', except this file
-Dir[File.join(__dir__, '*.rb')].each do |file|
-  require file unless file.end_with?('/badge.rb')
+    # Put your plugin logic here.
+    # The following variables are predefined:
+    #   @argument_string, @config, @envs, @helper, @layout, @logger, @mode, @page, @paginator, @site, @tag_name and @theme
+    #
+    # @param tag_name [String] is the name of the tag, which we already know.
+    # @param argument_string [String] the arguments from the web page.
+    # @param tokens [Liquid::ParseContext] tokenized command line
+    # @return [void]
+    def render_impl
+      @align = @helper.parameter_specified? 'align' # Obtain the value of parameter align
+      @class = @helper.parameter_specified? 'class' # Obtain the value of parameter class
+      @label = @helper.parameter_specified? 'label' # Obtain the value of parameter label
+      @style = @helper.parameter_specified? 'style' # Obtain the value of parameter style
+      <<~END_OUTPUT
+        <pre class="example">
+          @align='#{@align}'
+          @class='#{@class}'
+          @label='#{@label}'
+          @style='#{@style}'
+          Remaining markup: '#{@helper.remaining_markup}'.
+        </pre>
+      END_OUTPUT
+    end
+
+    JekyllPluginHelper.register(self, PLUGIN_NAME)
+  end
 end
-
-# Write the code for your gem here
